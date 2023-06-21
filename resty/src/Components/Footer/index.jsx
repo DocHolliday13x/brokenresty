9c46226ce660774add9585d4a1f164b383c22769
Ryan Eastman <rking1323@gmail.com>
2023-06-21 09:21:31 -0700

    changed classes to functions

--- resty/src/Components/Footer/index.jsx
+++ resty/src/Components/Footer/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+function Footer() {
+  return <footer>&copy; Uncle Buzz's Basement 2023</footer>;
 }
 
 export default Footer;
